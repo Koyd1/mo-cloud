bd9ff76419ded6dc78d358c8bb1c5d5e064adf0e
Alexandr <log.koyd@gmail.com>
2024-03-25 23:45:11 +0200

    stable v.2 Начало фронта

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,11 @@
 const Router = require("express");
-const User = require("../models/User")
-const bcrypt = require("bcryptjs")
-const {check, validationResult} = require("express-validator")
-const router = new Router()
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const {check, validationResult} = require("express-validator");
+const config = require("config");
+
+const router = new Router();
 
 
 router.post('/registration',
@@ -25,7 +28,7 @@ router.post('/registration',
             if(candidate) {
                 return res.status(400).json({message: `User with email ${email} already exist`})
             }
-            const hashPassword = await bcrypt.hash(password, 15)
+            const hashPassword = await bcrypt.hash(password, 8)
             const user = new User({email, password: hashPassword, usedSpace: 0})
             await user.save()
             return res.json({message: "User was created"})
@@ -36,5 +39,32 @@ router.post('/registration',
         }
     })
 
-
+router.post('/login',
+    async (req, res) => {
+        try {
+            const {email, password} = req.body
+            const user = await User.findOne({email})
+            if(!user) {
+                return res.status(404).json({message: "User not found"}) // 404 - not found
+            }
+            const isPassValid = bcrypt.compareSync(password, user.password) // сравниваем пароли
+            if(!isPassValid) {
+                return res.status(400).json({message: "Invalid password"}) // 400 - bad request
+            }
+            const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            res.send({message: "Server error"})
+        }
+    })
 module.exports = router
